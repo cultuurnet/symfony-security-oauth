c84b485645edd420ab4293b1f5f2344a0132de57
Nicolas Leroy <nicolas.leroy@gmail.com>
2015-09-02 12:43:32 +0200

    III-413: Modify the OAuthToken to include parameters from header

--- src/OAuthToken.php
+++ src/OAuthToken.php
@@ -34,16 +34,66 @@ class OAuthToken extends AbstractToken
      */
     public $user;
 
-    public function __construct(array $roles = array())
+    /**
+     * @var array
+     */
+    protected $requestParameters;
+    /**
+     * @var string
+     */
+    protected $requestMethod;
+    /**
+     * @var string
+     */
+    protected $requestUrl;
+
+    /**
+     * @param array $parameters An array of request parameters.
+     */
+    public function setRequestParameters($parameters)
     {
-        parent::__construct($roles);
-
-        // If the user has roles, consider it authenticated
-        $this->setAuthenticated(count($roles) > 0);
+        $this->requestParameters = $parameters;
     }
-
+    /**
+     * @return array An array of request parameters.
+     */
+    public function getRequestParameters()
+    {
+        return $this->requestParameters;
+    }
+    /**
+     * @param string $requestMethod A request method.
+     */
+    public function setRequestMethod($requestMethod)
+    {
+        $this->requestMethod = $requestMethod;
+    }
+    /**
+     * @return string The request method.
+     */
+    public function getRequestMethod()
+    {
+        return $this->requestMethod;
+    }
+    /**
+     * @param string $requestUrl A request URL.
+     */
+    public function setRequestUrl($requestUrl)
+    {
+        $this->requestUrl = $requestUrl;
+    }
+    /**
+     * @return string The request URL.
+     */
+    public function getRequestUrl()
+    {
+        return $this->requestUrl;
+    }
+    /**
+     * {@inheritdoc}
+     */
     public function getCredentials()
     {
-
+        return '';
     }
 }
