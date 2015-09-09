9f8755cd4fe7944704f09c92779643ea2c6e5299
Nicolas Leroy <nicolas.leroy@gmail.com>
2015-09-09 09:38:59 +0200

    III-413: Trim down ConsumerProviderInterface and TokenProviderInterface

--- src/Model/Provider/ConsumerProviderInterface.php
+++ src/Model/Provider/ConsumerProviderInterface.php
@@ -8,51 +8,11 @@
 
 namespace CultuurNet\SymfonySecurityOAuth\Model\Provider;
 
-use CultuurNet\SymfonySecurityOAuth\Model\ConsumerInterface;
-
 interface ConsumerProviderInterface
 {
-    /**
-     * Returns the consumer's fully qualified class name.
-     *
-     * @return string
-     */
-    public function getConsumerClass();
-
-    /**
-     * Create a consumer.
-     *
-     * @param string      $name
-     * @param string|null $callback
-     * @return \CultuurNet\SymfonySecurityOAuth\Model\ConsumerInterface
-     */
-    public function createConsumer($name, $callback = null);
-
-    /**
-     * @param array $criteria
-     * @return \CultuurNet\SymfonySecurityOAuth\Model\ConsumerInterface
-     */
-    public function getConsumerBy(array $criteria);
-
     /**
      * @param $consumerKey
      * @return \CultuurNet\SymfonySecurityOAuth\Model\ConsumerInterface
      */
     public function getConsumerByKey($consumerKey);
-
-    /**
-     * Deletes a consumer.
-     *
-     * @param ConsumerInterface $consumer
-     * @return void
-     */
-    public function deleteConsumer(ConsumerInterface $consumer);
-
-    /**
-     * Updates a consumer.
-     *
-     * @param ConsumerInterface $consumer
-     * @return void
-     */
-    public function updateConsumer(ConsumerInterface $consumer);
 }
