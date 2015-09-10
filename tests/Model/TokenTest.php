27ea82cd57070501a825b6831990ff8790d67009
Nicolas Leroy <nicolas.leroy@gmail.com>
2015-09-10 16:25:59 +0200

    III-413: Extra tests on token regarding expiry functionality

--- tests/Model/TokenTest.php
+++ tests/Model/TokenTest.php
@@ -30,10 +30,27 @@ class TokenTest extends \PHPUnit_Framework_TestCase
         $user = new UserMock('123456789', 'testUser', 'email@email.email');
 
         $token->setUser($user);
+        $expiresAt = time();
+        $token->setExpiresAt($expiresAt);
 
         $this->assertEquals('nnch734d00sl2jdk', $token->getToken(), 'The Token property is properly set');
         $this->assertEquals('pfkkdhi9sl3r4s00', $token->getSecret(), 'The Token Secret property is properly set');
         $this->assertEquals($consumer, $token->getConsumer(), 'The consumer property is properly set');
         $this->assertEquals($user, $token->getUser(), 'The user property is properly set');
+        $this->assertEquals($expiresAt, $token->getExpiresAt(), 'The user property is properly set');
+    }
+
+    public function testTokenExpireFunctionality()
+    {
+        $token = new Token();
+        $expiresAt = time();
+
+        $this->assertEquals(9223372036854775807, $token->getExpiresIn());
+
+        $token->setExpiresAt($expiresAt);
+        $expiresIn = $token->getExpiresIn();
+        $expiresInCalculated = $expiresAt - time();
+        $this->assertEquals($expiresIn, $expiresInCalculated, 'ExpiresIn gets calculated perfectly');
+        $this->assertFalse($token->hasExpired(), 'The token has net yet expired');
     }
 }
