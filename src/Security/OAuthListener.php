716bb2d33228619819b28a6f0f8b3ed67446f3e2
Nicolas Leroy <nicolas.leroy@gmail.com>
2015-09-15 10:43:13 +0200

    III-413: Remove UitidCredentials usage

--- src/Security/OAuthListener.php
+++ src/Security/OAuthListener.php
@@ -8,8 +8,6 @@
 
 namespace CultuurNet\SymfonySecurityOAuth\Security;
 
-use CultuurNet\Auth\ConsumerCredentials;
-use CultuurNet\UitidCredentials\UitidCredentialsFetcher;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
